03c86268c8d7af122e0a842b02a831be412d50cb
Mukhlis <mukhlismx4@gmail.com>
2026-06-17 22:40:35 +0800

    Updated styles and fixed layout

--- main/static/js/myScript2.js
+++ main/static/js/myScript2.js
@@ -21,7 +21,7 @@ const scholarshipContainer = document.getElementById("scholarshipContainer");
 async function loadScholarships() {
     try {
         // Fetch from your Django API root
-        const response = await fetch("http://127.0.0.1:8000/api/scholarships/");
+        const response = await fetch('https://yourusername.pythonanywhere.com/api/scholarships/');
         if (!response.ok) throw new Error("Network response was not ok");
         
         scholarshipData = await response.json();
